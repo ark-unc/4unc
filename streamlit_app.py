61a340ff9707f3a872eb40911cf14898de7c1293
ark-unc <ark.uncc@gmail.com>
2024-10-04 19:18:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,10 +47,11 @@ st.write("Selected Option is :", sub_cat_selected)
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 sales_by_month_cat = df.filter(items=['Sales','Sub_Category']).groupby(pd.Grouper(freq='M')).sum()
 
-sales_by_month_cat_1 = sales_by_month_cat.groupby('Sub_Category')
-st.write("Selected Option is :", sales_by_month_cat_1)
 
-#st.line_chart(sales_by_month_cat, y='Sales")
+#sales_by_month_cat_1 = sales_by_month_cat.groupby('Sub_Category')
+#st.write("Selected Option is :", sales_by_month_cat_1)
+
+st.line_chart(sales_by_month_cat, y='Sales", color="Sub_Category")
 #st.line_chart(df.groupby("Sub_Category", as_index=False).sum(), x="Sub_Category", y="Sales", color="#04f")
               
 st.write("## Your additions")
