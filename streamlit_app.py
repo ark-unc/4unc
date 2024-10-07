f6527a24d589aae14fe884a8e666f48edba8be0e
ark-unc <ark.uncc@gmail.com>
2024-10-07 11:58:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,9 +70,9 @@ st.write("Selected Sub-Cat Data for graph:", s2)
 st.write("### (4) show three metrics - total sales, total profit, and overall profit")
 
 test_metrics = test_df.groupby(['Sub_Category'])[["Sales","Profit"]].sum()
-st.write("METRICS")
-st.write(test_metrics)
-st.write("testing111")
+#st.write("METRICS")
+#st.write(test_metrics)
+#st.write("testing111")
 s3 = test_metrics.loc[sub_cat_selected]
 st.write("testing22222")
 overall_profit = test_metrics['Profit'].sum()
@@ -87,7 +87,7 @@ for ind in range(len(s3)):
     st.metric("Sub-Category " , sub_cat_selected[ind])
     st.metric("Sales ", s3.loc[sub_cat_selected[ind]][0])
     st.metric("Profit ", s3.loc[sub_cat_selected[ind]][1])
-    st.metric("Overall profit Margin %", ((s3.loc[sub_cat_selected[ind]][1])/(s3.loc[sub_cat_selected[ind]][0]))*100,delta=(overall_margin-((s3.loc[sub_cat_selected[ind]][1])/(s3.loc[sub_cat_selected[ind]][0]))))
+    st.metric("Overall profit Margin %", ((s3.loc[sub_cat_selected[ind]][1])/(s3.loc[sub_cat_selected[ind]][0]))*100,delta=(((s3.loc[sub_cat_selected[ind]][1])/(s3.loc[sub_cat_selected[ind]][0]))-overall_margin))
 
     #print("Sub-Cat" + t12[ind])
     #print("Sales" + str(t2.loc[t12[ind]][0]))
