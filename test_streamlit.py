3fbaf710b7490cbfbbf07f964c07fb156af0bd72
ark-unc <ark.uncc@gmail.com>
2024-10-07 22:45:32 -0400

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -80,7 +80,7 @@ st.write("111")
 #st.write(type(ss1))
 st.write(ss1.head(1))
 #st.write(ss1.get_loc())
-st.write(ss1.shape))
+st.write(ss1.describe())
 
 st.write("2222")
 st.write(ss1.get('Sub_Category'))
