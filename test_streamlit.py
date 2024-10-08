19cd9c1188d03a6f9e4af32c49ff3f315aca5fb3
ark-unc <ark.uncc@gmail.com>
2024-10-07 22:07:50 -0400

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -71,13 +71,14 @@ st.write(sales_by_sub_cat1)
 s2 = sales_by_sub_cat1.loc[sub_cat_selected]
 st.write("Selected Sub-Cat Data for graph:")
 st.dataframe(s2)
+s2.set_index('Order_Date', inplace=True)
 #s2 = sales_by_sub_cat.filter(sub_cat_selected)
 #s2.reset_index()
 #s2.set_index('Sub_Category', inplace=True)
-#st.write("111")
+st.write("111")
 #st.write(s2.loc['Sales'])
 #st.write("2222")
-#st.write(s2.loc['Sub_Category'])
+st.write(s2)
 
 
 #st.write("Selected Sub-Cat Data for graph:", s2)
